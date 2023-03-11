c1dc775fdd22a0b4ab3f9687922ae98de36d2def
Tormanov <106275740+Tormanov@users.noreply.github.com>
2023-03-11 18:50:57 +0200

    added test and start to package json  + database initialization

---
+++ src/config/databaseInit.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+async function initDatabase() {
+    mongoose.set('strictQuery', false)
+    await mongoose.connect('mongodb://127.0.0.1:27017/examPrep');
+
+};
+
+module.exports = initDatabase;
