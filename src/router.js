10e4320ceaa642393379601456212f08a47a9862
Tormanov <106275740+Tormanov@users.noreply.github.com>
2023-03-12 11:07:24 +0200

    added task controller to router

--- src/router.js
+++ src/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const employeeController = require('./controllers/employeeController');
+const taskController = require('./controllers/taskController');
 
 router.use(homeController);
 router.use('/employees', employeeController);
+router.use('/tasks', taskController);
 
 module.exports = router;
